8cdedf5240cc13fd7e9e4d794dee51b924ecf775
rupinder kaur <rupinder0007gill@gmail.com>
2020-08-21 06:06:19 +0530

    FriendlyId initialized
    bundle exec rails generate friendly_id
    bundle exec rubocop config/initializers/friendly_id.rb -a
    bundle exec rails db:migrate
    http://norman.github.io/friendly_id/

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_002824) do
+ActiveRecord::Schema.define(version: 2020_08_21_003427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -45,6 +45,17 @@ ActiveRecord::Schema.define(version: 2020_08_21_002824) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "versions", force: :cascade do |t|
